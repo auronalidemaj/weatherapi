fa35f897c589d8095a82b122ac9a3816cf63439e
auronalidemaj <auronalidemaj@gmail.com>
2023-05-01 23:18:48 +0200

    Weather API project

---
+++ scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = 'W9PFPI5IpDuIabycRDbvbfVSN0p3QRV8';
+// Weather Information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// City Information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// getCity('manchester').then(data => {
+//     return getWeather(data.Key);
+// }).then(data => {
+//     console.log(data);
+// })
+// .catch(err => console.log(err));
